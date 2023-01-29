3bafdd20e5db0e237848c5dac0def78b66ed1a7e
benjamin-fo <123658638+benjamin-fo@users.noreply.github.com>
2023-01-29 19:58:48 +0100

    Update changePass.php

--- guestbook/admin/includes/content/changePass.php
+++ guestbook/admin/includes/content/changePass.php
@@ -1,12 +1,12 @@
 <?php
-
+use guestbook\Error;
 if (@$magic != "0xDEADBEEF")
 	die("This file cannot be executed directly");
 	
 	echo '<div class="mainTitle">' . $lang['changePass'] . '</div>';
 	echo '<div class="helpPopup ' . $alignHelp . '"><a href="#" onclick="openHelp(\'changePass\');">' . $lang['help'] . '</a></div>';
 	
-	$submitId = secureVar($_POST['submit'], 'html');
+	$submitId = isset($_POST['submit']) ? secureVar($_POST['submit'], 'html') : '';
 	
 	if ((! empty($submitId)) && isset($submitId)) {
 		$passCheck['oldPassword'] = secureVar(trim($_POST['oldPassword']), 'html');
