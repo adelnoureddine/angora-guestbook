b4efb237fdb0cc88b5e926827c979f5e4ec8ba38
benjamin-fo <123658638+benjamin-fo@users.noreply.github.com>
2023-02-26 16:24:43 +0100

    Update phpinfo.php

--- guestbook/admin/includes/content/phpinfo.php
+++ guestbook/admin/includes/content/phpinfo.php
@@ -8,26 +8,20 @@ if (@$magic != "0xDEADBEEF")
 		die($error->showError());
 	}
 	
-	if (ob_start()) {
-		phpinfo();
+	ob_start();
+	phpinfo();
 	
-		preg_match ('%<style type="text/css">(.?)</style>.?<body>(.*?)</body>%s', ob_get_contents(), $matches);
+	preg_match ('%<style type="text/css">(.*?)</style>.*?<body>(.*?)</body>%s', ob_get_clean(), $matches);
 	
-		echo "<div class='phpinfodisplay'><style type='text/css'>\n",
-			join( "\n",
-				array_map(
-					create_function(
-						'$i',
-						'return ".phpinfodisplay " . preg_replace( "/,/", ",.phpinfodisplay ", $i );'
-						),
-					preg_split( '/\n/', $matches[1] )
-					)
-				),
-			"</style>\n",
-			$matches[2],
-			"\n</div>\n";
-	
-		ob_end_clean();
-	}
+	echo "<div class='phpinfodisplay'><style type='text/css'>\n",
+	    join( "\n",
+	        array_map(
+	            fn($i) => ".phpinfodisplay " . preg_replace("/,/", ",.phpinfodisplay ", $i),
+	            preg_split( '/\n/', $matches[1] )
+	            )
+	        ),
+	    "</style>\n",
+	    $matches[2],
+	    "\n</div>\n";
 
 ?>
